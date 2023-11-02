4a7b41bb0ef9aa1240a8e9e84fb1139b64807ec1
AhmadSJ <ahmad.jamalzada@gmail.com>
2023-11-02 10:24:01 +0100

    spring boot initializr

---
+++ src/test/java/com/ns/jokeservice/JokeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ns.jokeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
